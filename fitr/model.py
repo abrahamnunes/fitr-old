2102b1d44582e84737199590eb5cecc87586be01
abrahamnunes <abrahamjnunes@gmail.com>
2016-09-03 23:52:32 -0300

    Added model building module with parameter templates

---
+++ fitr/model.py
@@ -0,0 +1,22 @@
+'''
+    Includes functions for building models
+'''
+
+
+def learningrate():
+    return {1:'Learning Rate', 2:'unit'}
+
+def invtemp():
+    return {1:'Inverse Temperature', 2:'pos'}
+
+def discount():
+    return {1:'Discounting Rate', 2:'unit'}
+
+def etrace():
+    return {1:'Eligibility Trace', 2:'unit'}
+
+def rewardsens():
+    return {1:'Reward Sensitivity', 2:'unit'}
+
+def mbmfbalance():
+    return {1:'MB/MF Balance', 2:'unit'}
