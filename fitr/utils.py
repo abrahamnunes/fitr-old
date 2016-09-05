bc9af7d3442bd0f03b0da126ac3ecb0cba9aca21
abraham <nunes@dal.ca>
2016-09-05 14:04:05 -0300

    Added logsumexp function

--- fitr/utils.py
+++ fitr/utils.py
@@ -9,3 +9,17 @@ def softmax(x):
 def mnrandi(p):
     ''' Returns index of max value from a multinomial sample '''
     return np.argmax(rnd.multinomial(1, p))
+
+def logsumexp(x):
+    '''
+        - Protects against numerical overflow/underflow.
+        - Based on Samuel Gershman's `mfit` Matlab package:
+            - https://github.com/sjgershm/mfit
+    '''
+    ym = np.max(x)
+    yc = x - ym
+    y  = ym + np.log(np.sum(np.exp(yc)))
+    i  = np.argwhere(np.logical_not(np.isfinite(ym)))
+    if np.size(i) != 0:
+        y[i[0][0]] = ym[i[0][0]]
+    return y
